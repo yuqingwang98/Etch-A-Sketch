4cf0c59168602765556628e8a0f5d8619762633e
Yuqing Wang <yuqingwang42@gmail.com>
2022-12-28 19:00:06 +0000

    new design

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-function makeBlocks() {
-  for (var i = 0; i < 16; i++) {
-      var row = document.createElement('div');
-      row.className = "row";
-      for (var j = 0; j < 16; j++) {
-          var box = document.createElement('div');
-          box.className = "box";
-          row.appendChild(box);
-      }
-      document.getElementById('boxParent').appendChild(row);
-  }
-}
+// function makeBlocks() {
+//   for (var i = 0; i < 16; i++) {
+//       var row = document.createElement('div');
+//       row.className = "row";
+//       for (var j = 0; j < 16; j++) {
+//           var box = document.createElement('div');
+//           box.className = "box";
+//           row.appendChild(box);
+//       }
+//       document.getElementById('boxParent').appendChild(row);
+//   }
+// }
 
-makeBlocks();
+// makeBlocks();
