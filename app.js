24c3ddd1d3afeb7da8d392847650f7d59364e8e8
Yuqing Wang <yuqingwang42@gmail.com>
2022-12-28 20:45:36 +0000

    grid added

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+  let board = document.querySelector(".board");
+  board.style.gridTemplateColumns = "repeat(16, 1fr)";
+  board.style.gridTemplateRows = "repeat(16, 1fr)";
+})
+
 // function makeBlocks() {
 //   for (var i = 0; i < 16; i++) {
 //       var row = document.createElement('div');
