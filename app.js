0aee4e51484aa710a3ee47638ea1ecd05d49c69f
Yuqing Wang <yuqingwang42@gmail.com>
2022-12-28 21:03:42 +0000

    getSize() done

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
 document.addEventListener("DOMContentLoaded", function(){
-  let board = document.querySelector(".board");
-  board.style.gridTemplateColumns = "repeat(16, 1fr)";
-  board.style.gridTemplateRows = "repeat(16, 1fr)";
+  createBoard(16);
+
+  let btnPopup = document.querySelector("#popup");
+  btnPopup.addEventListener("click", function(){
+    let size = getSize();
+    createBoard(size);
+  })
 })
+
+function createBoard(size){
+  let board = document.querySelector(".board");
+
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let numDivs = size * size;
+
+  for (let i=0; i < numDivs; i++){
+    let div = document.createElement("div");
+    div.style.backgroundColor = 'yellow';
+    board.insertAdjacentElement('afterbegin', div);
+  }
+}
+
+function getSize(){
+  let choice = prompt("Choose the size you want");
+  let message = document.querySelector("#message")
+  if(choice == ""){
+      message.innerHTML = 'Please provide a number';
+  }
+  else if(choice < 0 || choice > 100){
+    message.innerHTML = 'Provide a number between 1 and 100';
+  }
+  else {
+    message.innerHTML = 'Board ready, enjoy';
+    return choice
+  }
+}
 
 // function makeBlocks() {
 //   for (var i = 0; i < 16; i++) {
